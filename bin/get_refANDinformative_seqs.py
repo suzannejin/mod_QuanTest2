04f5228d010cf1ec059c9a669cc670fe2a7d253b
suzannejin <suzanne.jin@crg.eu>
2020-04-02 17:20:51 +0200

    Modified get_refANDinformative_seqs.py script

--- bin/get_refANDinformative_seqs.py
+++ bin/get_refANDinformative_seqs.py
@@ -8,32 +8,84 @@ Warning: You should have T-coffee installed before running this script.
 
 '''
 
-def get_informative_fasta(fasta,tree,n):
 
-    ''' Get informative n sequences from a fasta file <msa or seq>, based on the guide tree <tree> 
+# ==========
+# FUNCTIONS 
+# ==========
+
+def read_fasta(filename):
+    ''' Read a FASTA file.
     
-    An ordered dictionary {name : sequence} with the n informative sequences is returned.
+    An ordered ditionary {name : sequence} is returned. 
     '''
 
     import collections
 
-    inf_fasta=collections.OrderedDict()
+    d=collections.OrderedDict()
+
+    with open(filename) as f:
+        for line in f:
+            line=line.strip("\n")
+            if line[0]==">":
+                name=line[1:]
+                d[name]=""
+            else:
+                d[name]+=line
+                
+    return(d)
+
+
+def read_aux(filename):
+    ''' Read the lines of a file.
+
+    '''
+    l=[]
+
+    with open(filename) as f:
+        for line in f:
+            line=line.strip("\n")
+            l.append(line)
+
+    return(l)
+
+
+def get_seqs(fasta,names):
+    ''' Given a FASTA file, get the specified sequences.
+
+    A ordered dictionary {name : sequence} with the specified sequences is returned.
+    '''
+
+    import collections
+
+    d=collections.OrderedDict()
+
+    for name in names:
+        seq=fasta[name]
+        d[name]=seq
+        
+    return(d)
+
+
+def get_informative_names(fastaname,tree,n):
+    ''' Get the name of the informative n sequences from a fasta file <msa or seq>, based on the guide tree <tree> 
     
-    tmp=os.popen("t_coffee -other_pg seq_reformat -in {} -in2 {} -action +regtrim {}".format(fasta,tree,n)).read()
+    An list with the name of the n informative sequences is returned.
+    '''
+
+    inf_names=[]
+    
+    tmp=os.popen("t_coffee -other_pg seq_reformat -in {} -in2 {} -action +regtrim {}".format(fastaname,tree,n)).read()
     tmp=[i for i in tmp.split(">") if i]
 
     for item in tmp:
-        item=[i for i in item.split("\n") if i]
+        item=[i for i in item.split("\n") if i] # Split into [ name, sequence ]
         name=item[0]
-        seq=""
-        for i in range(1,len(item)):
-            seq+=item[i]
-        inf_fasta[name]=seq
+        inf_names.append(name)
 
-    return(inf_fasta)
+    return(inf_names)
 
 
-def merge_ref_informative_seqs(ref_fasta,inf_fasta,n):
+def merge_ref_informative_seqs(msa,ref_names,inf_names,n):
     ''' Merge references + (n-ref) informative sequences 
 
     An ordered dictionary {name : sequence} with n sequences (ref + informative) is returned.
@@ -41,21 +93,29 @@ def merge_ref_informative_seqs(ref_fasta,inf_fasta,n):
 
     import collections
 
+    d=collections.OrderedDict()
+
     # Reference sequences
-    out=ref_fasta.copy()
+    for name in ref_names:
+        seq=msa[name]
+        d[name]=seq
+    
     # Add N-ref informative sequences
-    n=len(ref_fasta)
-    for name,seq in inf_fasta.items():
+    n=len(ref_names)
+    for name in inf_names:
         if n>=args.n:
             break
-        if name in ref_fasta.keys():
+        if name in ref_names:
             continue
-        out[name]=seq
+        d[name]=seq
         n+=1
     
-    return(out)
+    return(d)
 
 
+# =====
+# MAIN
+# =====
 
 if __name__ == '__main__':
 
@@ -67,7 +127,9 @@ if __name__ == '__main__':
    
 
     app = argparse.ArgumentParser(description=__description__)
-    app.add_argument('--fasta',type=str,help="Fasta file <seq or msa>")
+    app.add_argument('--seq',type=str,help="Sequences in FASTA format. \
+                     Note that the informative sequences will be retrieved based on this file (instead of the MSA in order to save time) and the guide tree")
+    app.add_argument('--msa',type=str,help="MSA in FASTA format")
     app.add_argument('--names',type=str,help="A file with a list of reference sequence names. \
                      In particular, a total of 3 sequences with known structures are assigned as the references \
                      (whose secondary structure will be predicted using QuanTest2)")
@@ -75,16 +137,16 @@ if __name__ == '__main__':
     app.add_argument('--n',type=int,help="Total number of sequences (ref + informative)")
     args = app.parse_args() 
 
-    # Read files
-    fasta=read_fasta(args.fasta)
-    names=read_aux(args.names)
-    
-    # Get reference sequences
-    ref_fasta=get_seqs(fasta,names)
-    # Get N-ref informative sequences
-    inf_fasta=get_informative_fasta(args.fasta,args.tree,args.n)
-    # Merge ref + informative sequences
-    out=merge_ref_informative_seqs(ref_fasta,inf_fasta,args.n)
+    # Read FASTA
+    seqs=read_fasta(args.seq)
+    msa=read_fasta(args.msa)
+
+    # Get reference sequence names
+    ref_names=read_aux(args.names)
+    # Get N informative sequence names
+    inf_names=get_informative_names(args.seq,args.tree,args.n)
+    # Merge ref + (N-ref) informative sequences
+    out=merge_ref_informative_seqs(msa,ref_names,inf_names,args.n)
 
     # Write output: references + (N-ref) informative sequences
     for name,seq in out.items():
